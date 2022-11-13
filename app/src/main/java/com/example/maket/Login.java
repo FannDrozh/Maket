f800c3e948b2d12e22a6d21a6a974d2cc6af123e
Илья Барашенков <ya.ilya-03@ya.ru>
2022-11-13 16:36:16 +0300

    0.4

--- app/src/main/java/com/example/maket/Login.java
+++ app/src/main/java/com/example/maket/Login.java
@@ -2,7 +2,9 @@ package com.example.maket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void LogCongrats(View v)
+    {
+        Intent intent = new Intent(this, Congrats.class);
+        startActivity(intent);
+    }
+    public void LogReg(View v)
+    {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
     }
 }
